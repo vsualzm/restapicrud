4ad96ca76c4d801d52cfe48a050e8efe673c913c
ilham maulana <ims.ilham@gmail.com>
2022-08-23 22:26:30 +0700

    second commit

---
+++ src/main/java/com/restapicrud/restapi/controllers/TestController.java
@@ -0,0 +1,16 @@
+package com.restapicrud.restapi.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// CRUD SEDERHANA
+@RestController
+@RequestMapping("/data")
+public class TestController {
+    @GetMapping("/user")
+    public String hello(){
+        return "TEST SPRING";
+    }
+}
